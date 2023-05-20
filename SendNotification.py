e224b5f6eaa1d3aec630f8b338df9e8145a87248
Serrindipity <jon.quitoriano@gmail.com>
2023-05-20 15:11:00 -0700

    hi

--- SendNotification.py
+++ SendNotification.py
@@ -4,6 +4,6 @@ def SendNotification(list forecasts):
     notification.notify(
         title = 'Weather Change!',
         message = 'The weather has changed from' + forecasts[0] + 'to' + forecasts[1],
-        app_icon = None,
-        timeout = 10,
+        app_icon = ./icon_final.ico,
+        timeout = 20,
     )
